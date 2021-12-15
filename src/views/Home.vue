cff609aeefe4403ade423c8b4fbef99c820e7cfd
rivalcoba <ivan.rivalcoba@gmail.com>
2021-12-14 18:57:07 -0600

    Remoción de páginas de destino individuales

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -49,4 +49,13 @@ img {
   display: flex;
   justify-content: space-between;
 }
+a {
+  color: lightseagreen;
+  text-decoration: none;
+}
+
+a:hover,
+a:visited {
+  text-decoration: underline;
+}
 </style>

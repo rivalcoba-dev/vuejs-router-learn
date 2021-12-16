11ce70ec4d83497ffca5ae01061a2d67fdb3d393
rivalcoba <ivan.rivalcoba@gmail.com>
2021-12-16 09:29:00 -0600

    Agregando Botón de retorno

--- src/views/DestinationDetails.vue
+++ src/views/DestinationDetails.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="">
+    <go-back />
     <section class="destination">
       <h1>{{ destination.name }}</h1>
       <div class="destination-details">
@@ -41,7 +42,11 @@
 </template>
 <script>
 import store from "@/store.js";
+import GoBack from "../components/GoBack.vue";
 export default {
+  components: {
+    GoBack,
+  },
   props: {
     slug: {
       type: String,
